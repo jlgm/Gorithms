34750750091aa94bf267f3c96b97b1087f92449c
jlgm <jlgmiran@gmail.com>
2021-07-12 19:57:10 -0300

    Add a simple Linked List implementation

---
+++ lists/linked_list.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type List struct {
+	Head *ListNode
+}
+
+func (this *List) PushBack(node *ListNode) {
+	if this.Head == nil {
+		this.Head = node
+	} else {
+		it := this.Head
+		for it.Next != nil {
+			it = it.Next
+		}
+		it.Next = node
+	}
+}
+
+func (this *List) PushFront(node *ListNode) {
+	if this.Head == nil {
+		this.Head = node
+	} else {
+		node.Next = this.Head
+		this.Head = node
+	}
+}
+
+func (this *List) PopFront() *ListNode {
+	if this.Head == nil {
+		return nil
+	}
+	it := this.Head
+	this.Head = this.Head.Next
+	return it
+}
+
+func (this *List) PopBack() *ListNode {
+	if this.Head == nil {
+		return nil
+	}
+	it := this.Head
+	prev := it
+	for it.Next != nil {
+		prev = it
+		it = it.Next
+	}
+	prev.Next = nil
+	return it
+}
+
+func (this *List) PrintList() {
+	it := this.Head
+	for it != nil {
+		fmt.Printf("%+v ", it.Val)
+		it = it.Next
+	}
+	fmt.Println()
+}
+
+func (this *List) PrintReverse(node *ListNode) {
+	if node == nil {
+		return
+	}
+	this.PrintReverse(node.Next)
+	fmt.Printf("%+v ", node.Val)
+	if node == this.Head {
+		fmt.Println()
+	}
+}
+
+func main() {
+	list := &List{}
+	list.PushBack(&ListNode{Val: 1})
+	list.PushBack(&ListNode{Val: 2})
+	list.PushBack(&ListNode{Val: 4})
+	list.PushBack(&ListNode{Val: 8})
+	list.PushFront(&ListNode{Val: -12})
+	list.PushBack(&ListNode{Val: 15})
+
+	list.PrintList()
+
+	fmt.Println("Removing from front:")
+	fmt.Println(list.PopFront().Val)
+	fmt.Println(list.PopFront().Val)
+
+	fmt.Println("Removing from back:")
+	fmt.Println(list.PopBack().Val)
+	fmt.Println(list.PopBack().Val)
+
+	list.PrintList()
+
+	list.PushBack(&ListNode{Val: 9})
+	list.PushBack(&ListNode{Val: 13})
+
+	list.PrintList()
+	list.PrintReverse(list.Head)
+
+}
